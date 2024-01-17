2f8c534fe67aab072c78d45a2f807c7b0d5d64de
VitorSoer <contato.soer@gmail.com>
2024-01-17 17:32:12 -0300

    feat: add new challenge

---
+++ Codewars/Javascript/8_kyu/15_switch_up.js
@@ -0,0 +1,20 @@
+/*
+    - When provided with a number between 0-9, return it in words.
+*/
+
+const switchItUp = (number) => {
+  const words = {
+    0: 'Zero',
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+  };
+
+  return words[number];
+};
